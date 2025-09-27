0a8447bb599233f59e747e8f8a18983908a64db7
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-09-27 19:05:11 +0600

    add remove css class using js

---
+++ js/styleInJs.js
@@ -0,0 +1,13 @@
+const sections = document.querySelectorAll('section');
+// console.log(sections);
+for (const section of sections) {
+    console.log(section);
+    section.style.border = '2px solid steelblue';
+    section.style.marginBottom = '10px';
+    section.style.borderRadius = '12px'
+    section.style.padding = '16px'
+    section.style.backgroundColor = 'lightgrey'
+}
+
+const placeSection = document.getElementById('place-section');
+placeSection.style.backgroundColor = 'yellow'
