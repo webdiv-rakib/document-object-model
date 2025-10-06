3dbae56c0c902c231bf96fe4808b41a7170c48ca
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-10-06 19:20:40 +0600

    3 options onclick

--- js-dom-events/js/events.js
+++ js-dom-events/js/events.js
@@ -1,1 +1,19 @@
 console.log('This is separate JS file');
+
+// System-2 (option-2)
+function makeGreen() {
+    document.body.style.backgroundColor = 'green';
+}
+
+// System-3 (option-3)
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+// System-4 (option-4)
+const makeYellowButton = document.getElementById('make-yellow');
+makeYellowButton.onclick = function makeYellow() {
+    document.body.style.backgroundColor = 'yellow';
+}
