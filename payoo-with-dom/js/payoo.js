7ffd2641fee0a445c00614b93b558c91c59cd7f9
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-10-16 04:56:33 +0600

    Add login form validation and event handling
    Introduced required attributes and IDs for phone and pin inputs in the login form. Added JavaScript to handle login button click, validate credentials, and provide feedback to the user.

--- payoo-with-dom/js/payoo.js
+++ payoo-with-dom/js/payoo.js
@@ -1,1 +1,14 @@
-console.log('Payoo Mobile Bank js connected');
+document.getElementById('log-in-btn').addEventListener('click', function (event) {
+    event.preventDefault();
+    // console.log('log in button clicked');
+    const phoneNumber = document.getElementById('phone-number').value;
+    // console.log(phoneNumber);
+    const pinCode = document.getElementById('password').value;
+    // console.log(pinCode);
+    if (phoneNumber === '5' && pinCode === '1234') {
+        console.log('Your are logged in');
+    }
+    else {
+        alert('Wrong phone or pin');
+    }
+})
