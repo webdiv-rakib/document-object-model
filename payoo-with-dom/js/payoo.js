03641005d9087962bb89616c7047006c56fdc037
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-10-16 05:24:11 +0600

    Add home page and redirect after login
    Introduces home.html as a new landing page. Updates payoo.js to redirect users to home.html upon successful login. Minor formatting adjustments in index.html for input fields.

--- payoo-with-dom/js/payoo.js
+++ payoo-with-dom/js/payoo.js
@@ -6,6 +6,12 @@ document.getElementById('log-in-btn').addEventListener('click', function (event)
 
     if (phoneNumber === '5' && pinNumber === '12345') {
         console.log('You are logged in')
+
+        //to open in same tab
+        window.location.href = 'home.html'; 
+
+        // to open in new tab
+        // window.open('home.html', '_blank');
     }
     else {
         alert('Wrong phone number or pin code');
