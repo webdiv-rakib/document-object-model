eb03039b957acfb41a049f823f63c89f5d7cb5a4
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-10-16 21:32:29 +0600

    Add event handler for Add Money form submission
    Assigned IDs to Add Money form inputs and button in home.html. Added a click event listener in payoo-home.js to log the entered amount and pin number, preparing for further form handling.

--- payoo-with-dom/js/payoo-home.js
+++ payoo-with-dom/js/payoo-home.js
@@ -1,3 +1,12 @@
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
+    event.preventDefault();
+    const addMoneyInput = document.getElementById('add-money-input').value;
+    console.log(addMoneyInput);
+    const pinNumber = document.getElementById('pin-number-input').value;
+    console.log(pinNumber);
+})
+
+// Extra Section below
 document.getElementById('add-money').addEventListener('click', function () {
     const addMoneySection = document.getElementById('add-money-section');
     addMoneySection.classList.toggle('hidden');
