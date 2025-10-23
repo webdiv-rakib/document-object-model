321475393fd3eafbe0cdceecca8efb0af44bc5e3
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2025-10-23 20:45:04 +0600

    still working on add money function

--- payoo-recap/js/add-money.js
+++ payoo-recap/js/add-money.js
@@ -3,4 +3,15 @@ document.getElementById('btn-add-money').addEventListener('click', function (eve
     const addMoney = document.getElementById('add-money-input').value;
     const pinNumber = document.getElementById('pin-number-input').value;
     console.log(addMoney, pinNumber);
+
+    if (pinNumber === '123') {
+        const balance = document.getElementById('current-balance').innerText;
+        const addMoneyNumber = parseFloat(addMoney);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = balanceNumber + addMoneyNumber;
+        document.getElementById('current-balance').innerText = newBalance;
+    }
+    else {
+        alert('Something went wrong');
+    }
 })
