3d66b561b4502968013fa05c414b3621a5b81e7f
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-10-24 19:38:27 +0600

    working on common function that we use often

--- payoo-recap/js/add-money.js
+++ payoo-recap/js/add-money.js
@@ -1,17 +1,24 @@
+// document.getElementById('btn-add-money').addEventListener('click', function (event) {
+//     event.preventDefault();
+//     const addMoney = document.getElementById('add-money-input').value;
+//     const pinNumber = document.getElementById('pin-number-input').value;
+//     console.log(addMoney, pinNumber);
+
+//     if (pinNumber === '123') {
+//         const balance = document.getElementById('current-balance').innerText;
+//         const addMoneyNumber = parseFloat(addMoney);
+//         const balanceNumber = parseFloat(balance);
+//         const newBalance = balanceNumber + addMoneyNumber;
+//         document.getElementById('current-balance').innerText = newBalance;
+//     }
+//     else {
+//         alert('Something went wrong');
+//     }
+// })
+console.log('add money loaded');
 document.getElementById('btn-add-money').addEventListener('click', function (event) {
     event.preventDefault();
-    const addMoney = document.getElementById('add-money-input').value;
-    const pinNumber = document.getElementById('pin-number-input').value;
-    console.log(addMoney, pinNumber);
-
-    if (pinNumber === '123') {
-        const balance = document.getElementById('current-balance').innerText;
-        const addMoneyNumber = parseFloat(addMoney);
-        const balanceNumber = parseFloat(balance);
-        const newBalance = balanceNumber + addMoneyNumber;
-        document.getElementById('current-balance').innerText = newBalance;
-    }
-    else {
-        alert('Something went wrong');
-    }
+    console.log('add money button clicked');
+    // const addMoney = document.getElementById('add-money-input').value;
+    // const addMoneyNumber = parseFloat(addMoney);
 })
---
+++ payoo-recap/js/utilities.js
@@ -0,0 +1,8 @@
+/**
+ * Common Shared Functions here
+ */
+console.log('Utilities loaded');
+
+function getInputFieldValueById() {
+    console.log('will get input value');
+}
