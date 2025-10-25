301d81b023d666212ce793c6e8272e21fdc93ddf
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-10-25 23:35:55 +0600

    add-money practice using function complete

--- payoo-recap/js/add-money.js
+++ payoo-recap/js/add-money.js
@@ -18,6 +18,16 @@
 // console.log('add money loaded');
 
 // ============Using Function to do this===============>>>>>
-document.getElementById('').addEventListener('click', function (event) {
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
     event.preventDefault();
+    const inputAddMoney = getInputFieldValue('add-money-input')
+    const accountBalance = getCurrentBalanceText('current-balance')
+    const pinCode = getInputPinCode('pin-number-input');
+    if (pinCode === '123') {
+        const newBalance = accountBalance + inputAddMoney;
+        document.getElementById('current-balance').innerText = newBalance;
+    }
+    else {
+        alert('Something went wrong');
+    }
 })
--- payoo-recap/js/utilities.js
+++ payoo-recap/js/utilities.js
@@ -21,3 +21,20 @@
 // }
 
 // Use common function to do
+
+function getCurrentBalanceText(id) {
+    const accountBalance = document.getElementById(id).innerText;
+    const accountBalanceNumber = parseFloat(accountBalance);
+    return accountBalanceNumber;
+}
+
+function getInputFieldValue(id) {
+    const addMoney = document.getElementById(id).value;
+    const addMoneyNumber = parseFloat(addMoney);
+    return addMoneyNumber;
+}
+
+function getInputPinCode(id) {
+    const pinCode = document.getElementById(id).value;
+    return pinCode;
+}
