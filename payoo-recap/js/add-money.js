65db5d877c8bc2b9e434b79ccce3964888b126e6
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2025-10-31 22:16:31 +0600

    another comment added

--- payoo-recap/js/add-money.js
+++ payoo-recap/js/add-money.js
@@ -33,3 +33,4 @@ document.getElementById('btn-add-money').addEventListener('click', function (eve
 })
 // add money section practice done
 // will start working from today
+// another useless day spend with out any work
