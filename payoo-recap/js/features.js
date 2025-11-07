36be29a074eb9639fed03b794fcfd9591ddc2d4b
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-11-07 20:57:12 +0600

    toggle feature added

--- payoo-recap/js/features.js
+++ payoo-recap/js/features.js
@@ -1,3 +1,11 @@
 document.getElementById('add-money').addEventListener('click', function () {
+    showASectionById('add-money-section');
+})
+
+document.getElementById('cash-out').addEventListener('click', function () {
     showASectionById('cashout-money-section');
+})
+
+document.getElementById('transfer-money').addEventListener('click', function () {
+    showASectionById('transfer-section');
 })
--- payoo-recap/js/utilities.js
+++ payoo-recap/js/utilities.js
@@ -1,27 +1,4 @@
-/**
- * Common Shared Functions here
- */
-// console.log('Utilities loaded');
-
-// function getInputFieldValueById() {
-//     console.log('will get input value');
-//     const addMoney = document.getElementById('add-money-input').value;
-//     return addMoney;
-// }
-
-// function getInputValue(id) {
-//     const inputValue = document.getElementById(id).value;
-//     const inputNumber = parseFloat(inputValue);
-//     return inputNumber;
-// }
-// function getCurrentBalance(id) {
-//     const currentBalance = document.getElementById(id).innerText;
-//     const currentBalanceNumber = parseFloat(currentBalance);
-//     return currentBalanceNumber;
-// }
-
 // Use common function to do
-
 function getCurrentBalanceText(id) {
     const accountBalance = document.getElementById(id).innerText;
     const accountBalanceNumber = parseFloat(accountBalance);
@@ -40,7 +17,14 @@ function getInputPinCode(id) {
 }
 
 function showASectionById(id) {
-    const addMoneyForm = document.getElementById(id);
-    
-    return addMoneyForm;
+
+    // hide all the sections first
+    document.getElementById('add-money-section').classList.add('hidden');
+    document.getElementById('cashout-money-section').classList.add('hidden');
+    document.getElementById('transfer-section').classList.add('hidden');
+
+    // show the section with the provide id as parameter
+    document.getElementById(id).classList.remove('hidden');
+    document.getElementById(id).classList.remove('hidden');
+    document.getElementById(id).classList.remove('hidden');
 }
