1daa50aa189809b4220e696df03ae5fbf3189d6b
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-11-20 21:53:34 +0600

    dynamic way to show transaction history

--- payoo-recap/js/add-money.js
+++ payoo-recap/js/add-money.js
@@ -7,6 +7,13 @@ document.getElementById('btn-add-money').addEventListener('click', function (eve
     if (pinCode === '123') {
         const newBalance = accountBalance + inputAddMoney;
         document.getElementById('current-balance').innerText = newBalance;
+
+        // add to transaction history
+        const p = document.createElement('p');
+        p.innerText = `Added: ${inputAddMoney} Tk. Balance: ${newBalance}`;
+
+        // should be a common function to show history
+        document.getElementById('transaction-container').appendChild(p);
     }
     else {
         alert('Something went wrong');
