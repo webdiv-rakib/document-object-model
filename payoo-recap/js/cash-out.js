75ac90d5f52c192c32c4344f8d79b090563743ca
webdiv-rakib <mdrakibulhassan24@gmail.com>
2025-11-21 04:07:25 +0600

    cashout history shows in transaction history section

--- payoo-recap/js/cash-out.js
+++ payoo-recap/js/cash-out.js
@@ -7,6 +7,10 @@ document.getElementById('btn-cashout-money').addEventListener('click', function 
     if (cashOutPin === '12345') {
         const newBalance = currentBalance - cashOutAmount;
         document.getElementById('current-balance').innerText = newBalance;
+
+        const p = document.createElement('p');
+        p.innerText = `Cash-out amount: ${cashOutAmount} Tk, Current Balance: ${newBalance}`;
+        document.getElementById('transaction-container').appendChild(p);
     }
     else {
         alert('Something went wrong');
